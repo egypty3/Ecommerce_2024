7664a924cdab2c3761919cce50c0ccac7d1b4579
Tarek Mamdouh <egypty3@gmail.com>
2024-11-03 23:34:29 +0200

    + RepositoryPattern ProductRepository class + RepositoryPattern IProductRepository interface + custom Exception class

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public  interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IList<Product>> GetProductsAsync();
+        Task<IList<ProductBrand>> GetProductBrandsAsync();
+        Task<IList<ProductType>> GetProductTypesAsync();
+    }
+}
---
+++ Infrastructure/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly EcommerceContext _context;
+
+        public ProductRepository(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return _context
+                .Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .FirstOrDefault(p => p.Id == id) 
+                ?? throw new NotFoundException("Product not found");
+
+        }
+
+        public async Task<IList<Product>> GetProductsAsync()
+        {
+            return 
+                await _context
+                .Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .ToListAsync();
+        }
+        public Task<IList<ProductBrand>> GetProductBrandsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<ProductType>> GetProductTypesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
